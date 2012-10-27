526839f7b3b49d4f5c6b8d0786f76410846ff354
Jai Kumar <jai.kumar@memeo-inc.com>
2012-10-26 17:13:38 -0700

    initial commit

--- api/app.rb
+++ api/app.rb
@@ -3,9 +3,9 @@ require "sinatra/json"
 
 # Database-less model for sample purposes only
 $products = [
-  { id: 1, name: "Product One", desc: "a description of product one" },
-  { id: 2, name: "Product Two", desc: "a description of product two" },
-  { id: 3, name: "Product Three", desc: "a description of product three" }
+  { id: 1, name: "Product One", desc: "a description of product one", country: "brazil", price: 200},
+  { id: 2, name: "Product Two", desc: "a description of product two", country: "us", price: 300 },
+  { id: 3, name: "Product Three", desc: "a description of product three", country: "brazil", price: 200 }
 ]
 
 get "/products.json" do
